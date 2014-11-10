7ea8d90ff815475f4cedc037ec2afdc82944fdf6
Charlie Egan <charlieegan3@googlemail.com>
2014-11-10 10:40:03 +0000

    Update Splitter module to return sentence hashes

--- splitter.rb
+++ splitter.rb
@@ -7,19 +7,22 @@ module Splitter
   # Relative average time 0.002076s
   def Splitter.punkt_extract_sentences(text)
     tokenizer = Punkt::SentenceTokenizer.new(text)
-    tokenizer.sentences_from_text(text, :output => :sentences_text)
+    sentences = tokenizer.sentences_from_text(text, :output => :sentences_text)
+    sentences.each_with_index.map { |v, i| {index: i, text: v} }
   end
 
   # WARNING: tactful_tokenizer is massively slower, 250 times slower...
   # Relative average time = 0.495286s
   def Splitter.tactful_extract_sentences(text)
     m = TactfulTokenizer::Model.new
-    m.tokenize_text(text)
+    sentences = m.tokenize_text(text)
+    sentences.each_with_index.map { |v, i| {index: i, text: v} }
   end
 
   # WARNING: Treat is marginally less consistent for very complex texts
   # Relative average time = 0.0249s
   def Splitter.treat_extract_sentences(text)
-    Treat::Entities::Paragraph.new(text).segment.to_a
+    sentences = Treat::Entities::Paragraph.new(text).segment.to_a
+    sentences.each_with_index.map { |v, i| {index: i, text: v} }
   end
 end
