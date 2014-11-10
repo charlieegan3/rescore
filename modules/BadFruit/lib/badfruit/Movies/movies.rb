d716d3b92d10712a39ac0833e97d5e5b79ad143b
Charlie Egan <charlieegan3@googlemail.com>
2014-11-10 15:07:15 +0000

    Re-Include Dylans BadFruit fork, my bad

---
+++ modules/BadFruit/lib/badfruit/Movies/movies.rb
@@ -0,0 +1,38 @@
+module BadFruit
+  class Movies
+    MAX_PAGE_LIMIT = 50
+    
+    def initialize(badfruit)
+      @badfruit = badfruit
+    end
+    
+    #returns an array of movie objects from the given search result.
+    def search_by_name(name, page_limit=1, page=1)
+      if page_limit > 50
+        page_limit = MAX_PAGE_LIMIT #current limitation of the rotten tomatos API
+      end
+      return @badfruit.parse_movies_array(JSON.parse(@badfruit.search_movies(name, page_limit, page)))
+    end
+
+    # search by id
+    def search_by_id(movie_id)
+      movie = @badfruit.get_movie_info(movie_id, "main")
+      raise 'Movie not found' if movie.nil? || movie.empty?
+      @badfruit.parse_movie_array(JSON.parse(movie))
+    end
+
+    # search by alias (currently only IMDB is supported)
+    def search_by_alias(alias_id,type='imdb')
+      movie = @badfruit.get_movie_alias_info(alias_id, type)
+      json = JSON.parse(movie)
+      raise 'Movie not found' if !json['error'].nil?
+      @badfruit.parse_movie_array(json)
+    end
+
+    # similar movie
+    def similar_movies(movie_id)
+      return @badfruit.parse_movies_array(JSON.parse(@badfruit.similar_movies(movie_id)))
+    end
+
+  end
+end
