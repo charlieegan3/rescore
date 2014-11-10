2144aba7f1c2330eaa3f6da7def7b34169c5e3ae
Charlie Egan <charlieegan3@googlemail.com>
2014-11-10 10:34:32 +0000

    Implement Sentiment Module

---
+++ sentiment.rb
@@ -0,0 +1,26 @@
+require 'simple_sentiment'
+require 'sad_panda'
+require 'sentiment_lib'
+require 'sentimental'
+
+module Sentiment
+  class Sentiment::SentimentAnalyzer
+    def initialize()
+      Sentimental.load_defaults
+      @sentimental_analyzer = Sentimental.new
+      @sentimentlib_analyzer = SentimentLib::Analyzer.new(:strategy => ::SentimentLib::Analysis::Strategies::BasicDictStrategy.new)
+    end
+
+    def get_sentiment(sentence)
+      scores = []
+      scores << (SimpleSentiment.sentiment(sentence).score.to_f / 10).round(2) rescue scores << 0
+      scores << ((SadPanda.polarity(sentence) - 5).to_f / 6).round(2)
+      scores << @sentimentlib_analyzer.analyze(sentence).to_f.round(2) / 2
+      scores << @sentimental_analyzer.get_score(sentence).to_f.round(2) / 2
+      total = scores.reduce(:+).to_f
+      average = total / 4
+      corrected_average = ((total - scores.sort_by {|x| (average - x).abs }.last) / 4).round(2)
+      {average: average, corrected_average: corrected_average, scores: scores, total: total}
+    end
+  end
+end
