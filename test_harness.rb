2282e6b0aa228af2018daf7f45eedb504ac0b8d9
Dylan Hanning <dylanhanning123@hotmail.com>
2014-11-30 20:35:18 +0000

    add colour to test_harness time output, and do argument checking

--- test_harness.rb
+++ test_harness.rb
@@ -7,6 +7,12 @@ print 'Reading From:'.colorize(:white).on_black.underline
 puts directory = 'reviews/movie'
 
 if ARGV[0] == "time"
+  begin
+  Integer(ARGV[1])
+  rescue
+    puts "Enter no. of reviews to parse as second argument.".colorize(:red)
+    exit
+  end
 
   start_time = Time.now
   count = 0
@@ -45,31 +51,29 @@ if ARGV[0] == "time"
     times_populate_related_people.push(times[:extract_sentences])
     times_apply_people_tags.push(times[:extract_sentences])
     times_get_emphasis.push(times[:extract_sentences])
-
-    puts "-------\n"
   end
 
-  puts "> Times taken for extract_sentences (average: #{times_extract_sentences.inject(:+) / times_extract_sentences.length}): \n #{times_extract_sentences}"
+  puts "> Times taken for extract_sentences (average: #{times_extract_sentences.inject(:+) / times_extract_sentences.length}): \n #{times_extract_sentences}".colorize(:blue)
 
-  puts "> Times taken for include_cleaned_sentences (average: #{times_include_cleaned_sentences.inject(:+) / times_include_cleaned_sentences.length}): \n #{times_include_cleaned_sentences}"
+  puts "> Times taken for include_cleaned_sentences (average: #{times_include_cleaned_sentences.inject(:+) / times_include_cleaned_sentences.length}): \n #{times_include_cleaned_sentences}".colorize(:blue)
 
-  puts "> Times taken for evaluate_sentiment (average: #{times_evaluate_sentiment.inject(:+) / times_evaluate_sentiment.length}): \n #{times_evaluate_sentiment}"
+  puts "> Times taken for evaluate_sentiment (average: #{times_evaluate_sentiment.inject(:+) / times_evaluate_sentiment.length}): \n #{times_evaluate_sentiment}".colorize(:blue)
 
-  puts "> Times taken for apply_context_tags (average: #{times_apply_context_tags.inject(:+) / times_apply_context_tags.length}): \n #{times_apply_context_tags}"
+  puts "> Times taken for apply_context_tags (average: #{times_apply_context_tags.inject(:+) / times_apply_context_tags.length}): \n #{times_apply_context_tags}".colorize(:blue)
 
-  puts "> Times taken for apply_noun_phrases (average: #{times_apply_noun_phrases.inject(:+) / times_apply_noun_phrases.length}): \n #{times_apply_noun_phrases}"
+  puts "> Times taken for apply_noun_phrases (average: #{times_apply_noun_phrases.inject(:+) / times_apply_noun_phrases.length}): \n #{times_apply_noun_phrases}".colorize(:blue)
 
-  puts "> Times taken for guess_film_name_from_text (average: #{times_guess_film_name_from_text.inject(:+) / times_guess_film_name_from_text.length}): \n #{times_guess_film_name_from_text}"
+  puts "> Times taken for guess_film_name_from_text (average: #{times_guess_film_name_from_text.inject(:+) / times_guess_film_name_from_text.length}): \n #{times_guess_film_name_from_text}".colorize(:blue)
 
-  puts "> Times taken for populate_related_people (average: #{times_populate_related_people.inject(:+) / times_populate_related_people.length}): \n #{times_populate_related_people}"
+  puts "> Times taken for populate_related_people (average: #{times_populate_related_people.inject(:+) / times_populate_related_people.length}): \n #{times_populate_related_people}".colorize(:blue)
 
-  puts "> Times taken for apply_people_tags (average: #{times_apply_people_tags.inject(:+) / times_apply_people_tags.length}): \n #{times_apply_people_tags}"
+  puts "> Times taken for apply_people_tags (average: #{times_apply_people_tags.inject(:+) / times_apply_people_tags.length}): \n #{times_apply_people_tags}".colorize(:blue)
 
-  puts "> Times taken for get_emphasis (average: #{times_get_emphasis.inject(:+) / times_get_emphasis.length}): \n #{times_get_emphasis}"
+  puts "> Times taken for get_emphasis (average: #{times_get_emphasis.inject(:+) / times_get_emphasis.length}): \n #{times_get_emphasis}".colorize(:blue)
 
-  puts "\n\n > TOTAL TIME FOR #{ARGV[1]} reviews: #{Time.now - start_time}"
+  puts "\n\n > TOTAL TIME FOR #{ARGV[1]} reviews: #{Time.now - start_time}".colorize(:blue)
 
-else
+elsif ARGV[0] == "step"
   Dir.foreach(directory) do |item|
     next if item == '.' or item == '..'
     content = File.readlines("#{directory}/#{item}").last
@@ -89,4 +93,9 @@ else
     gets
     system("clear")
   end
+
+else
+  puts "Run me as 'ruby test_harness.rb time [no. of reviews to parse]'".colorize(:red)
+  puts "or 'ruby test_harness.rb step'".colorize(:red)
+  exit
 end
