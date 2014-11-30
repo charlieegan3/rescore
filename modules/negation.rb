ee9c6d81c7db0da57c4b984dac227a5c014c7f00
Dylan Hanning <dylanhanning123@hotmail.com>
2014-11-30 14:29:44 +0000

    migrate negation to ruby module

---
+++ modules/negation.rb
@@ -0,0 +1,15 @@
+require 'simple_sentiment'
+require 'sad_panda'
+require 'sentiment_lib'
+require 'sentimental'
+
+module Negation
+  def Negation.tag_negation(sentence)
+    negative = "no,not,Nobody,Nothing,Neither,Nowhere,Never,n't,Hardly,scarcely,barely,but,instead" 
+    sentence.split().each do |word|
+      if negative.include?(word)
+        puts word 
+      end
+    end
+  end
+end
