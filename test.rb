f6417a9b92343282dd374876143bc7c0107439c6
Dylan Hanning <dylanhanning123@hotmail.com>
2014-12-02 22:21:05 +0000

    whatever

--- test.rb
+++ test.rb
@@ -4,24 +4,25 @@ require 'colorize'
 require_relative 'review'
 
 print 'Reading From:'.colorize(:white).on_black.underline
-puts directory = 'reviews/movie'
+puts directory = 'demo_reviews'
 
 Dir.foreach(directory) do |item|
+
   next if item == '.' or item == '..'
-  content = File.readlines("#{directory}/#{item}").last
-  next if content.count('.') < 10
+  content = File.readlines("#{directory}/#{item}").join(" ")
+  p content
 
-  review = Review.new(content)
-  review.build_all
-  puts review.text
-  review.sentences.each do |sentence|
-    puts '-' * 50
-    sentence.map {|k,v| print k; p v}
-    gets
-  end
-  puts review.film_name
-  puts review.related_people
+  # review = Review.new(content)
+  # review.build_all
+  # puts review.text
+  # review.sentences.each do |sentence|
+  #   puts '-' * 50
+  #   sentence.map {|k,v| print k; p v}
+  #   gets
+  # end
+  # puts review.film_name
+  # puts review.related_people
 
-  gets
-  system("clear")
+  # gets
+  # system("clear")
 end
