bf6794ede8647b03dc8c0c2bfe38651050bb9923
Charlie Egan <charlieegan3@googlemail.com>
2014-12-09 22:07:10 +0000

    Update searcher to pull all reviews

--- scrapers/searcher.rb
+++ scrapers/searcher.rb
@@ -1,3 +1,8 @@
+require_relative 'amazon_scraper'
+require_relative 'rt_scraper'
+require_relative 'metacritic_scraper'
+require_relative 'imdb_scraper'
+
 require 'googleajax'
 require 'open-uri'
 
@@ -5,8 +10,13 @@ GoogleAjax.referrer = "www.resco.re"
 
 film_title = "The Hobbit: An Unexpected Journey"
 
-p GoogleAjax::Search.web(film_title + " imdb")[:results][0][:unescaped_url]
-p GoogleAjax::Search.web(film_title + " rotten tomatoes")[:results][0][:unescaped_url]
-p GoogleAjax::Search.web(film_title + " metacritic")[:results][0][:unescaped_url]
-p GoogleAjax::Search.web(film_title + " amazon.com product reviews")[:results][0][:unescaped_url]
-p GoogleAjax::Search.web(film_title + " empire.com review")[:results][0][:unescaped_url]
+puts metacritic_title_url = GoogleAjax::Search.web(film_title + " metacritic")[:results][0][:unescaped_url]
+puts amazon_title_url = GoogleAjax::Search.web(film_title + " amazon.com product reviews")[:results][0][:unescaped_url]
+puts imdb_title_url = GoogleAjax::Search.web(film_title + " imdb")[:results][0][:unescaped_url]
+puts rotten_title_url = GoogleAjax::Search.web(film_title + " rotten tomatoes")[:results][0][:unescaped_url]
+# empire_title_url = GoogleAjax::Search.web(film_title + " empire.com review")[:results][0][:unescaped_url]
+
+p MetacriticScraper.scrape_reviews(metacritic_title_url).size
+p AmazonScraper.scrape_reviews(amazon_title_url).size
+p IMDbScraper.scrape_reviews(imdb_title_url).size
+p RtScraper.scrape_reviews(rotten_title_url).size
