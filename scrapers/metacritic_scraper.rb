a4d41d57bc0feb60ca1068a9d5ceaf89fc250a59
Charlie Egan <charlieegan3@googlemail.com>
2014-12-09 20:19:02 +0000

    Implement Metacritic Scraper

--- scrapers/metacritic_scraper.rb
+++ scrapers/metacritic_scraper.rb
@@ -1,48 +1,68 @@
 require 'nokogiri'
 require 'open-uri'
-require 'securerandom'
-require 'pry'
 
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.854.0 Safari/535.2"
-OUTPUT_FOLDER = 'movie'
-
-PAGES = [
-  'http://www.metacritic.com/movie/gone-girl/user-reviews',
-  'http://www.metacritic.com/movie/the-lord-of-the-rings-the-return-of-the-king/user-reviews'
-]
-
-reviews = [].tap do |reviews|
-  PAGES.each do |url|
-    xml = Nokogiri::HTML(open(url, 'User-Agent' => USER_AGENT).read)
-    xml.css('.critic_reviews_module').first.remove
-    xml.css('.review').to_a.map { |review| reviews << review }
-  end
-end
-
-reviews.each do |review|
-  total_ups = review.css('.total_ups').first.text.to_f
-  total_thumbs = review.css('.total_thumbs').first.text.to_i
-  quality_score = total_ups / total_thumbs
-
-  if quality_score.to_s.include? 'NaN'
-    quality_score = 0
+module MetacriticScraper
+  USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.854.0 Safari/535.2"
+  MAX_PAGES = 3
+  def page_count(review_url)
+    doc = Nokogiri::HTML(open(review_url, 'User-Agent' => USER_AGENT).read)
+    pages = doc.css('#tn15content table')[1].css('td').first.text.gsub(':','')[/\d+$/].to_i
+    pages = MAX_PAGES if pages > MAX_PAGES
   end
 
-  user_score = review.css('.review_grade').first.text.gsub(/\s/,"").to_i
+  def review_urls(title_url)
+    review_url = title_url + '/user-reviews'
+    doc = Nokogiri::HTML(open(review_url, 'User-Agent' => USER_AGENT).read)
+    total_pages = doc.css('.page.last_page').text.to_i
+    total_pages = MAX_PAGES if total_pages > MAX_PAGES
 
-  review_text = ""
-  if review.css('.review_body .blurb').size > 0
-    review.css('.review_body .blurb').each do |blurb|
-      review_text += blurb.text.gsub(/\s+/," ")
+    urls = []
+    for i in 0..MAX_PAGES
+      urls << review_url + "?num_items=100&page=#{i}"
     end
-  else
-    review_text = review.css('.review_body').text.gsub(/\s+/," ")
+    urls
   end
 
-  File.open("reviews/#{OUTPUT_FOLDER}/#{SecureRandom.hex[0..10]}.txt", 'w') { |file|
-    file.write("#{user_score}\n#{quality_score}\n#{review_text}")
-  }
+  def raw_reviews(review_url)
+    xml = Nokogiri::HTML(open(review_url, 'User-Agent' => USER_AGENT).read)
+    xml.css('.critic_reviews_module').first.remove
+    xml.css('.review').to_a
+  end
+
+  def evaluate_useful(raw_review)
+    total_ups = raw_review.css('.total_ups').first.text.to_f
+    total_thumbs = raw_review.css('.total_thumbs').first.text.to_i
+    [total_ups, total_thumbs]
+  end
+
+  def extract_content(raw_review)
+    content = ""
+    if raw_review.css('.review_body .blurb').size > 0
+      raw_review.css('.review_body .blurb').each do |blurb|
+        content += blurb.text.gsub(/\s+/," ")
+      end
+    else
+      content = raw_review.css('.review_body').text.gsub(/\s+/," ")
+    end
+    content
+  end
+
+  def scrape_reviews(title_url)
+    reviews = []
+
+    review_urls(title_url).each do |review_url|
+      raw_reviews(review_url).each do |raw_review|
+        review_hash = {}
+        review_hash[:rating] = raw_review.css('.review_grade').first.text.gsub(/\s/,"").to_i
+        review_hash[:content] = extract_content(raw_review)
+        review_hash[:useful] = evaluate_useful(raw_review)
+        review_hash[:username] = raw_review.css('.name').text.strip
+        review_hash[:date] = raw_review.css('.date').text.strip
+        review_hash[:location] = nil
+        review_hash[:title] = nil
+        reviews.push(review_hash)
+      end
+    end
+    reviews
+  end
 end
-
-
-
