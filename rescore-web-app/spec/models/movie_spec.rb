8f36c8d8e8ad56bed6633274bac487d8a6991e03
Charlie Egan <charlieegan3@googlemail.com>
2014-12-21 15:46:11 +0000

    Add sample to movie_spec

--- rescore-web-app/spec/models/movie_spec.rb
+++ rescore-web-app/spec/models/movie_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Movie, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should get source links' do
+    movie = Movie.new(title: 'Lord of the Rings: The Fellowship of the Ring')
+    VCR.use_cassette('populate_source_links') do
+      movie.populate_source_links
+    end
+    expect(movie.imdb_link).to_not be_nil
+    expect(movie.amazon_link).to_not be_nil
+    expect(movie.metacritic_link).to_not be_nil
+    expect(movie.rt_link).to_not be_nil
+  end
 end
