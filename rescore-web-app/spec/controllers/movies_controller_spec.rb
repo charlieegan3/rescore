46ce23d06ecd4e1e18dd36d4437b4ae8904ba3d1
Charlie Egan <charlieegan3@googlemail.com>
2014-12-21 16:36:35 +0000

    Add token view spec

--- rescore-web-app/spec/controllers/movies_controller_spec.rb
+++ rescore-web-app/spec/controllers/movies_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe MoviesController, :type => :controller do
-
+  describe 'index' do
+    it 'builds a list of movies' do
+      movie = Movie.create(title: 'Lord of the Rings')
+      get :index
+      expect(response).to render_template('index')
+      expect(assigns(:movies)).to match_array([movie])
+    end
+  end
 end
---
+++ rescore-web-app/spec/views/movies/index.html.erb_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "movies/index", :type => :view do
+  it "renders list of movies" do
+    assign(:movies, [Movie.create(title: 'lord of the rings')])
+    render
+    expect(rendered).to include('lord of the rings')
+  end
+end
