28708dc4c3264dc506075075b350990f67394dc9
Charlie Egan <charlieegan3@googlemail.com>
2014-12-21 11:25:18 +0000

    Implement foundation Movie CRUD

--- rescore-web-app/app/controllers/movies_controller.rb
+++ rescore-web-app/app/controllers/movies_controller.rb
@@ -1,2 +1,38 @@
 class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def create
+    @movie = Movie.create(movie_params)
+    redirect_to movie_path(@movie)
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update_attributes(movie_params)
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    Movie.find(params[:id]).delete
+    redirect_to movies_path
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(:title)
+    end
 end
