00c7f6960ea54ed0ce2fc7cd0bb7637fd2033656
Charlie Egan <charlieegan3@googlemail.com>
2014-12-21 17:24:17 +0000

    Remove proof of concept residue

--- old/sentiment/py_sentiment.py
+++
@@ -1,9 +0,0 @@
-import sys
-from textblob import TextBlob
-
-text = sys.argv[1]
-
-blob = TextBlob(text)
-
-print blob.sentences[0].sentiment.polarity, ',', blob.sentences[0].sentiment.subjectivity
-
