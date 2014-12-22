7084c35e0a66af0e1522553a7dc514b5b514f3cc
Charlie Egan <charlieegan3@googlemail.com>
2014-12-22 20:45:45 +0000

    Clean up metadata remains

--- rescore-web-app/app/controllers/metadata_controller.rb
+++
@@ -1,7 +0,0 @@
-class MetadataController < ApplicationController
-  def search
-    bf = BadFruit.new("6tuqnhbh49jqzngmyy78n8v3")
-    @movies = []
-    @movies = bf.movies.search_by_name(params[:query]) if params[:query]
-  end
-end
