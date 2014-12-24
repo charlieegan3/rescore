a258a80654624af144f03c80dfde34ef3e2e8fe4
Charlie Egan <charlieegan3@googlemail.com>
2014-12-24 00:57:32 +0000

    Include foundation CSS

--- rescore-web-app/app/assets/javascripts/application.js
+++ rescore-web-app/app/assets/javascripts/application.js
@@ -12,7 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require turbolinks
 //= require_tree .
 
 //= require highcharts/highcharts
+
+$(function(){ $(document).foundation(); });
