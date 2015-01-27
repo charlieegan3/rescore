19545bcc594465da878549861ae73b50e2bdc5e9
Charlie Egan <charlieegan3@googlemail.com>
2015-01-27 22:15:46 +0000

    Correct form behavior for other pages

--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,12 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   get 'new_from_lookup' => 'movies#new_from_lookup', as: :new_movie_from_lookup
+  get '/movies/search_by_title' => 'movies#search_by_title'
   get 'movies/:id/populate_source_links' => 'movies#populate_source_links', as: :populate_movie_source_links
   get 'movies/:id/populate_related_people' => 'movies#populate_related_people', as: :populate_movie_related_people
   get 'movies/:id/collect' => 'movies#collect', as: :collect_movie
   get 'movies/:id/status' => 'movies#status', as: :movie_status
   get 'movies/:id/build_summary' => 'movies#build_summary', as: :build_movie_summary
-  get '/search_by_title' => 'movies#search_by_title'
   resources :movies
 
   # Example resource route with options:
