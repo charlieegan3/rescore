d70b2fc80778058505896f0a968c7efbb38d27c5
Dylan Hanning <dylanhanning123@hotmail.com>
2015-01-31 15:10:33 +0000

    simplify people and utils slightly

--- lib/modules/utils.rb
+++ lib/modules/utils.rb
@@ -3,9 +3,10 @@ module Utils
     indexes = []
     original = sentence.dup
     while 1
-      if sentence.index(key) != nil
-        indexes.push(sentence.index(key) + (original.length - sentence.length))
-        sentence[sentence.index(key)..sentence.index(key) + (key.length-1)] = ''
+      i = sentence.index(key)
+      if i != nil
+        indexes.push(i + (original.length - sentence.length))
+        sentence[i..i + (key.length-1)] = ''
       else
         break
       end
