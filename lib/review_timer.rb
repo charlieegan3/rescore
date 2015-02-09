bae2eb3df4234786924889811bca881c0f5a311b
worldinmyeyes <wojciechszczerek@wp.pl>
2015-02-09 16:44:35 +0000

    nothing

--- lib/review_timer.rb
+++ lib/review_timer.rb
@@ -1,10 +1,11 @@
 require_relative '../config/environment'
 Delayed::Worker.delay_jobs = false
 
-movie = Movie.last
+movie = Movie.find(5)
 puts movie.title
 start = Time.now
 
+binding.pry
 movie.build_summary
 
 puts "build_summary completed in #{Time.now - start} for #{movie.reviews.size} reviews"
