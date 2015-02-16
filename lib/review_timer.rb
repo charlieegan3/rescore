e5249c41250ea1b3895bf1772a422519707adf7f
Charlie Egan <charlieegan3@googlemail.com>
2015-02-16 20:44:50 +0000

    Remove noise from branch starting point

--- lib/review_timer.rb
+++ lib/review_timer.rb
@@ -1,11 +1,10 @@
 require_relative '../config/environment'
 Delayed::Worker.delay_jobs = false
 
-movie = Movie.find(5)
+movie = Movie.last
 puts movie.title
 start = Time.now
 
-binding.pry
 movie.build_summary
 
 puts "build_summary completed in #{Time.now - start} for #{movie.reviews.size} reviews"
