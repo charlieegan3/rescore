b940ff991f94301451eff9dd0f919e72f768a5d9
Dylan Hanning <dylanhanning123@hotmail.com>
2015-02-18 14:25:17 +0000

    Add capybara, factory girl, and movie search test

---
+++ spec/features/movies_search_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+require 'spec_helper'
+require 'vcr'
+
+Delayed::Worker.delay_jobs = false
+
+RSpec.describe MoviesController, :type => :feature do
+  describe 'search_by_title' do
+    it 'returns the correct result' do
+      # VCR.turn_off!
+      # WebMock.allow_net_connect!
+      movie = FactoryGirl::create(:movie)
+
+      visit('/')
+    
+      within("#search_form") do
+        fill_in 'query', :with => 'The Hobbit'
+      end
+
+      click_button 'movie_search_submit'
+      expect(page).to have_content 'The Hobbit'
+      # VCR.turn_on!
+    end
+  end
+end
