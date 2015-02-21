ddf85e821761f6e24228f5a9377d7390cebc9bb0
Charlie Egan <charlieegan3@googlemail.com>
2015-02-21 17:07:20 +0000

    Tidy annotation_assistant

--- corpus/annotation_assistant.rb
+++ corpus/annotation_assistant.rb
@@ -1,101 +1,5 @@
-require_relative '../../config/environment'
-
-keywords = {
-  editing: {
-    'editing' => 100,
-    'effects' => 70,
-    'post production' => 50
-  },
-
-  sound: {
-    'sound' => 100,
-    'music' => 100,
-    'dolby' => 100,
-    'audio' => 100,
-    'sounds' => 100,
-    'surround' => 80,
-    'ears' => 20,
-    'score' => 10
-  },
-
-  plot: {
-    'story' => 100,
-    'plot' => 100,
-    'narrative' => 80,
-    'predictable' => 80,
-    'narration' => 80,
-    'tale' => 30,
-    'characters' => 50,
-    'character' => 50,
-    'journey' => 30,
-    'persona' => 20
-  },
-
-  dialog: {
-    'dialog' => 100,
-    'dialogue' => 100,
-    'speech' => 100,
-    'spoken' => 90,
-    'discussion' => 80,
-    'conversation' => 80,
-    'lines' => 70,
-    'language' => 70,
-    'delivery' => 50
-  },
-
-  cast: {
-    'acting' => 100,
-    'cast' => 100,
-    'performance' => 90,
-    'portrayal' => 90,
-    'depiction' => 90,
-    'characterization' => 90,
-    'impersonation' => 90,
-    'role' => 70,
-    'persona' => 50
-  },
-
-  vision: {
-    'visuals' => 100,
-    'imax' => 100,
-    'cgi' => 100,
-    '3d' => 100,
-    'graphics' => 100,
-    'visual' => 100,
-    'graphic' => 90,
-    'colour' => 90,
-    'color' => 90,
-    'vision' => 90,
-    'blurry' => 80,
-    'lifelike' => 80,
-    'screen' => 70,
-    'spectacle' => 60,
-    'eyes' => 30,
-    'beautiful' => 20,
-    'designs' => 8
-  },
-
-  length: {
-    'dragged' => 100,
-    'bloated' => 70,
-    'boring' => 50,
-    'long' => 50
-  },
-
-  credibility: {
-    'true to the book' => 100,
-    'believable' => 100,
-    'improbable' => 100,
-    'far-fetched' => 100,
-    'implausible' => 100,
-    'true' => 80,
-    'truthful' => 80,
-    'truthfully' => 70,
-    'honest' => 70
-  }
-}
-
-
+require_relative '../config/environment'
+require_relative 'keywords'
 
 review = {}
 
@@ -133,7 +37,7 @@ Punkt::SentenceTokenizer.new(review[:body]).
     aspects[:movie] = [spotted]
   end
 
-  keywords.each do |k, v|
+  KEYWORDS.each do |k, v|
     v.keys.each do |term|
       aspects[k] += [term] if s.include? term
     end
@@ -150,7 +54,7 @@ Punkt::SentenceTokenizer.new(review[:body]).
   end
 
   puts 'Is the list missing an aspect thats mentioned? If so, type it below'.red
-  puts 'Remember, ONLY one of these: '.black_on_red + keywords.keys.join(" ")
+  puts 'Remember, ONLY one of these: '.black_on_red + KEYWORDS.keys.join(" ")
   puts 'Otherwise, enter to continue...'.green
   input = ''
   loop do
