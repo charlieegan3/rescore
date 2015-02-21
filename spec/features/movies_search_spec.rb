81c047d648309b258300476de869ae056e5dd125
Dylan Hanning <dylanhanning123@hotmail.com>
2015-02-21 12:46:31 +0000

    Add test for movie manage page

--- spec/features/movies_search_spec.rb
+++ spec/features/movies_search_spec.rb
@@ -5,14 +5,20 @@ require 'vcr'
 Delayed::Worker.delay_jobs = false
 
 RSpec.describe MoviesController, :type => :feature do
-  #before(:each) do
+  before(:each) do
+    movie = FactoryGirl::create(:movie)
   #  stub_template "application/_topics_graph.html.erb" => "This content"
-  #end
+  end
+
+  describe 'manage' do
+    it 'lists the existing films' do
+      visit ('/movies')
+      expect(page).to have_content 'Gladiator'
+    end
+  end
 
   describe 'search_by_title' do
     it 'returns the correct result' do
-      movie = FactoryGirl::create(:movie)
-
       visit('/movies/search_by_title?query=gladiator')
     
       #within("#search_form") do
