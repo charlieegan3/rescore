b99e0c99c6d05fad723886f8a994b94e24ff296d
Dylan Hanning <dylanhanning123@hotmail.com>
2015-02-22 16:42:57 +0000

    Revert to old capybara test

--- spec/features/movies_search_spec.rb
+++ spec/features/movies_search_spec.rb
@@ -7,7 +7,6 @@ Delayed::Worker.delay_jobs = false
 RSpec.describe MoviesController, :type => :feature do
   before(:each) do
     movie = FactoryGirl::create(:movie)
-  #  stub_template "application/_topics_graph.html.erb" => "This content"
   end
 
   describe 'manage' do
@@ -19,13 +18,13 @@ RSpec.describe MoviesController, :type => :feature do
 
   describe 'search_by_title' do
     it 'returns the correct result' do
-      visit('/movies/search_by_title?query=gladiator')
-    
-      #within("#search_form") do
-      #  fill_in 'query', :with => 'Gladiator'
-      #end
+      visit('/')
 
-      #click_button 'movie_search_submit'
+      within("#search_form") do
+       fill_in 'query', :with => 'Gladiator'
+      end
+
+      click_button 'movie_search_submit'
       expect(page).to have_content 'Gladiator'
     end
   end
