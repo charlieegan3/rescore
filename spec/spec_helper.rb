d9ac3ef50da9b5055d35fd92e327cf14fd37b64b
Dylan Hanning <dylanhanning123@hotmail.com>
2015-02-23 17:36:00 +0000

    Add color to Rspec output by default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,8 @@ RSpec.configure do |config|
     FactoryGirl.reload
   end
 
+  config.color = true
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
