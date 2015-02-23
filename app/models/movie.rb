9f8e3fc016599e1b6189764088731f27d1722067
Charlie Egan <charlieegan3@googlemail.com>
2015-02-23 21:33:31 +0000

    Refactor Movie set_sentiment

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -58,63 +58,14 @@ class Movie < ActiveRecord::Base
   end
 
   def set_sentiment
-    topics_sentiment  = Hash[ASPECTS.map {|k,v| [k, []]}]
-    people_sentiment  = {}
-    average_sentiment = []
-    sentiment_averages = [] #this is review level
-    reviews.each do |review|
-      next if review[:rescore_review].nil? || review[:rescore_review].empty?
-      sentiment_average = 0
-      review[:rescore_review].each do |sentence|
-        average_sentiment << sentence[:sentiment]
-        sentiment_average += sentence[:sentiment]
-        sentence[:context_tags].keys.each do |tag|
-          topics_sentiment[tag] << sentence[:sentiment] * sentence[:context_tags][tag]
-        end
-        sentence[:people_tags].each do |tag|
-          people_sentiment[tag] = [] if people_sentiment[tag].nil?
-          people_sentiment[tag] << sentence[:sentiment]
-        end
-      end
-      sentiment_average /= review[:rescore_review].size
-      sentiment_averages << sentiment_average
-    end
-
-    topics_sentiment = topics_sentiment.map { |k, v| [k, v.reduce(:+) / v.size] }
-    people_sentiment = people_sentiment.sort_by { |_, v| v.size }
-    people_sentiment = people_sentiment.
-      map { |k, v| [k, v.reduce(:+) / v.size, v.size] }.reverse.
-      reject { |_, _, c| c < 3}.
-      sort_by { |_, v, c| (v * 100).to_f / c }.reverse
-
-    average_sentiment = dup_hash(average_sentiment.map {|x| x.round(1)}).to_a.sort_by { |x| x[0] }
-    mean = average_sentiment.map { |x| x[0] }.mean
-    standard_deviation = average_sentiment.map { |x| x[0] }.standard_deviation
-    average_sentiment.map! { |x| [((x[0] - mean) / standard_deviation).round(2), x[1]] }
-    labels = [0, average_sentiment.size - 1, average_sentiment.size / 2, average_sentiment.size / 4, average_sentiment.size - average_sentiment.size / 4]
-    average_sentiment.each_with_index do |x, i|
-      average_sentiment[i] = ['', x[1]] unless labels.include? i
-    end
-
-    distribution_stats = [sentiment_averages.max - sentiment_averages.min, sentiment_averages.standard_deviation]
-
-    # collect locations and average sentiment
-    location_sentiment = reviews.map {|x| [x[:location], x[:rescore_review].map {|x| x[:sentiment]}.mean]}
-    # reject inappropriate locations
-    location_sentiment.reject! { |e| e.first == '' || e.first.nil? }
-    # group locations and select groups greater than one in size
-    location_sentiment = location_sentiment.group_by { |e| e[0]}.to_a.select { |e| e[1].size > 1 }
-    # order and calculate scores for groups, sort groups by size
-    location_sentiment = location_sentiment.map { |e| [e[0], e[1].map { |e2| e2[1] }.reduce(:+), e[1].size ] }.sort_by { |e| e[1] }.reverse
-    # tidy names and score values
-    location_sentiment = location_sentiment.map { |e| [e[0].gsub('from ', ''), e[1].round(2) * 100, e[2]] }
-
+    s = SentimentCalculator.new(reviews)
+    s.build
     {
-      topics: topics_sentiment,
-      people: people_sentiment,
-      distribution: average_sentiment,
-      location: location_sentiment,
-      distribution_stats: distribution_stats
+      topics: s.topics_sentiment,
+      people: s.people_sentiment,
+      distribution: s.sentence_sentiment,
+      location: s.location_sentiment,
+      distribution_stats: s.review_sentiment
     }
   end
 
@@ -138,10 +89,4 @@ class Movie < ActiveRecord::Base
   def self.fast_find(id)
     where(id: id).select('id, title, image_url, year, genres, related_people, sentiment, stats, updated_at, created_at').first
   end
-
-  private
-    def dup_hash(ary)
-     ary.inject(Hash.new(0)) { |h,e| h[e] += 1; h }.select {
-     |k,v| v > 1 }.inject({}) { |r, e| r[e.first] = e.last; r }
-    end
 end
---
+++ lib/sentiment_calculator.rb
@@ -0,0 +1,64 @@
+class SentimentCalculator
+  def initialize(reviews)
+    @reviews = reviews
+
+    @topics_sentiment = Hash[ASPECTS.map {|k,v| [k, []]}]
+    @people_sentiment = Hash.new([])
+    @sentence_sentiment = []
+    @review_sentiment = []
+  end
+
+  def build
+    @reviews.each do |review|
+      next if review[:rescore_review].nil? || review[:rescore_review].empty?
+      review[:rescore_review].each do |s|
+        @sentence_sentiment << s[:sentiment]
+        s[:context_tags].keys.each { |tag| @topics_sentiment[tag] << s[:sentiment] * s[:context_tags][tag] }
+        s[:people_tags].each { |tag| @people_sentiment[tag] << s[:sentiment] }
+      end
+      @review_sentiment << @sentence_sentiment.last(review[:rescore_review].size).reduce(:+) / review[:rescore_review].size
+    end
+  end
+
+  def topics_sentiment
+    @topics_sentiment.map { |k, v| [k, v.reduce(:+) / v.size] }
+  end
+
+  def people_sentiment
+    @people_sentiment = @people_sentiment.sort_by { |_, v| v.size }
+    @people_sentiment = @people_sentiment.
+      map { |k, v| [k, v.reduce(:+) / v.size, v.size] }.reverse.
+      reject { |_, _, c| c < 3}.
+      sort_by { |_, v, c| (v * 100).to_f / c }.reverse
+  end
+
+  def sentence_sentiment
+    @sentence_sentiment = dup_hash(@sentence_sentiment.map {|x| x.round(1)}).to_a.sort_by { |x| x[0] }
+    mean = @sentence_sentiment.map { |x| x[0] }.mean
+    standard_deviation = @sentence_sentiment.map { |x| x[0] }.standard_deviation
+    @sentence_sentiment.map! { |x| [((x[0] - mean) / standard_deviation).round(2), x[1]] }
+    labels = [0, @sentence_sentiment.size - 1, @sentence_sentiment.size / 2, @sentence_sentiment.size / 4, @sentence_sentiment.size - @sentence_sentiment.size / 4]
+    @sentence_sentiment.each_with_index do |x, i|
+      @sentence_sentiment[i] = ['', x[1]] unless labels.include? i
+    end
+    @sentence_sentiment
+  end
+
+  def review_sentiment
+    [@review_sentiment.max - @review_sentiment.min, @review_sentiment.standard_deviation]
+  end
+
+  def location_sentiment
+    sentiment = @reviews.map {|x| [x[:location], x[:rescore_review].map {|x| x[:sentiment]}.mean]}
+    sentiment.reject! { |e| e.first == '' || e.first.nil? }
+    sentiment = sentiment.group_by { |e| e[0]}.to_a.select { |e| e[1].size > 1 }
+    sentiment = sentiment.map { |e| [e[0], e[1].map { |e2| e2[1] }.reduce(:+), e[1].size ] }.sort_by { |e| e[1] }.reverse
+    sentiment.map { |e| [e[0].gsub('from ', ''), e[1].round(2) * 100, e[2]] }
+  end
+
+  private
+    def dup_hash(ary)
+     ary.inject(Hash.new(0)) { |h,e| h[e] += 1; h }.select {
+     |k,v| v > 1 }.inject({}) { |r, e| r[e.first] = e.last; r }
+    end
+end
