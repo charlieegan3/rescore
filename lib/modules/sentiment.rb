1ab45dd5c78f1d68a84ec7526ea40c1808cb7e1b
Charlie Egan <charlieegan3@googlemail.com>
2015-02-23 10:10:01 +0000

    Update sentiment splitting

--- lib/modules/sentiment.rb
+++ lib/modules/sentiment.rb
@@ -1,7 +1,7 @@
 module Sentiment
   def Sentiment.evaluate_sentence(sentence)
     score = 0
-    sentence.split(/\s+/).each do |word|
+    sentence.split(/\W+/).each do |word|
       score += POLARITIES[word] if POLARITIES[word].present?
     end
     score
