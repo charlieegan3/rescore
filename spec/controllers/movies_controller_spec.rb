09baf63c9ff7b9fe77e475fe9f8aa7754ca6dfcf
Dylan Hanning <dylanhanning123@hotmail.com>
2015-02-23 19:54:36 +0000

    Add compare method spec

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,12 +1,27 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe MoviesController, :type => :controller do
+
   describe 'index' do
     it 'builds a list of movies' do
-      movie = Movie.create(title: 'Lord of the Rings')
+      movie = FactoryGirl::create(:movie) # before(:each) doesn't put 'movie' in scope here...
       get :index
       expect(response).to render_template('index')
       expect(assigns(:movies)).to match_array([movie])
+    end
+  end
+
+  describe 'compare' do
+    it 'redirects if there are less than two movies' do
+      get :compare
+      expect(response).to render_template('/')
+    end
+
+    it 'displays properly if there are at least two movies' do
+      movie2 = FactoryGirl::create(:movie, title: "The Hobbit")
+      get :compare
+      expect(response).to render_template('movies/choose_compare')
     end
   end
 end
