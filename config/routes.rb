b4bc2b14dcf7553a6195ef74d42582ded6f50617
Charlie Egan <charlieegan3@googlemail.com>
2015-02-25 20:52:08 +0000

    Reorder routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,13 +15,13 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   get 'new_from_lookup' => 'movies#new_from_lookup', as: :new_movie_from_lookup
   get '/movies/search_by_title' => 'movies#search_by_title'
+  get 'movies/compare' => 'movies#compare', as: :compare_movies
+  get 'movies/:id/status' => 'movies#status', as: :movie_status
   get 'movies/:id/manage' => 'movies#manage', as: :manage_movie
   get 'movies/:id/populate_source_links' => 'movies#populate_source_links', as: :populate_movie_source_links
   get 'movies/:id/populate_related_people' => 'movies#populate_related_people', as: :populate_movie_related_people
   get 'movies/:id/collect' => 'movies#collect', as: :collect_movie
-  get 'movies/:id/status' => 'movies#status', as: :movie_status
   get 'movies/:id/build_summary' => 'movies#build_summary', as: :build_movie_summary
-  get 'movies/compare' => 'movies#compare', as: :compare_movies
   resources :movies
 
   # Example resource route with options:
