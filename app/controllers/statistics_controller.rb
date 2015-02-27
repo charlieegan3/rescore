eb76c43d04b062e370dd660ffdac45bc5c5dbbfb
Dylan Hanning <dylanhanning123@hotmail.com>
2015-02-27 14:11:27 +0000

    Add couple tests for stats

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,6 +1,10 @@
 class StatisticsController < ApplicationController
   def index
     Statistic.refresh if params[:update]
+    if Statistic.count == 0
+      flash[:alert] = "We don't have any statistics right now, check back later!"
+      return redirect_to :root
+    end
 
     @review_count = Statistic.find_by_identifier('review_count').value[:count]
     @people_count = Statistic.find_by_identifier('people_count').value[:count]
