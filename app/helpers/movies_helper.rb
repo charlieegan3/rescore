9575430bf49575c57963666b22c77d8493bb363a
Dylan Hanning <dylanhanning123@hotmail.com>
2015-02-28 13:47:36 +0000

    Cleanup compare method

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,23 +1,20 @@
 module MoviesHelper
 
-  def comparison_summary(movie_one, movie_two)
-    m1 = movie_one
-    m2 = movie_two
-    winners = {:sentiment => :both, :review_controversy => :both}
+  def comparison_summary(movie_1, movie_2)
+    winners = {overall_sentiment: :both, review_controversy: :both}
 
-    # Aspect sentiment.
-    m1.sentiment[:topics].each do |topic|
-      if m2.sentiment[:topics][topic[0]] && topic[1] > m2.sentiment[:topics][topic[0]]
-        winners[:sentiment] = m1.title
-      elsif m2.sentiment[:topics][topic[0]] && topic[1] < m2.sentiment[:topics][topic[0]]
-        winners[:sentiment] = m2.title
+    movie_1.sentiment[:topics].each do |topic|
+      if movie_2.sentiment[:topics][topic[0]] && topic[1] > movie_2.sentiment[:topics][topic[0]]
+        winners[:overall_sentiment] = movie_1.title
+      elsif movie_2.sentiment[:topics][topic[0]] && topic[1] < movie_2.sentiment[:topics][topic[0]]
+        winners[:overall_sentiment] = movie_2.title
       end
     end
 
-    if m2.sentiment[:distribution_stats][:st_dev] > m1.sentiment[:distribution_stats][:st_dev]
-      winners[:review_controversy] = m2.title
-    elsif m2.sentiment[:distribution_stats][:st_dev] < m1.sentiment[:distribution_stats][:st_dev]
-      winners[:review_controversy] = m1.title
+    if movie_2.sentiment[:distribution_stats][:st_dev] > movie_1.sentiment[:distribution_stats][:st_dev]
+      winners[:review_controversy] = movie_2.title
+    elsif movie_2.sentiment[:distribution_stats][:st_dev] < movie_1.sentiment[:distribution_stats][:st_dev]
+      winners[:review_controversy] = movie_1.title
     end
 
     return winners
