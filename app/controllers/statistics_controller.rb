b8afb5338b085c1deb9746e67dbb31d2ba32b4cf
Charlie Egan <charlieegan3@googlemail.com>
2015-02-28 23:13:03 +0000

    Add link to refresh statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,16 +1,17 @@
 class StatisticsController < ApplicationController
   def index
-    if Statistic.count == 0
-      flash[:alert] = "We don't have any statistics right now, check back later!"
-      return redirect_to :root
-    end
-
-    Statistic.refresh if params[:update]
+    Statistic.refresh if Statistic.count == 0
 
     @review_count = Statistic.find_by_identifier('review_count').value[:count]
     @people_count = Statistic.find_by_identifier('people_count').value[:count]
 
     @aspects = Statistic.find_by_identifier('topic_sentiments').value
     @counts = Statistic.find_by_identifier('topic_counts').value
+  end
+
+  def refresh
+    Statistic.refresh
+    flash[:alert] = "Stats Updated"
+    redirect_to :movie_admin
   end
 end
