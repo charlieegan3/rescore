d0d2b5abde1dab97d9404ca35c1647446e74333b
Dylan Hanning <dylanhanning123@hotmail.com>
2015-03-02 13:10:27 +0000

    Add badfruit api key constant

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -55,7 +55,7 @@ class MoviesController < ApplicationController
   end
 
   def new_from_lookup
-    bf = BadFruit.new("6tuqnhbh49jqzngmyy78n8v3")
+    bf = BadFruit.new(Bf.key)
     @movies = []
     @movies = bf.movies.search_by_name(params[:query], 3) if params[:query]
     @movies.each do |movie|
---
+++ lib/bf.rb
@@ -0,0 +1,5 @@
+module Bf
+  def Bf.key
+    "6tuqnhbh49jqzngmyy78n8v3"
+  end
+end
