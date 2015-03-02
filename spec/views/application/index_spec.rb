44f3bda110b5cc9efea90c578b19c74e7900e21b
Georgi <g_babachev@yahoo.co.uk>
2015-03-02 15:15:35 +0000

    Adding a test for the homepage(recent movie)

---
+++ spec/views/application/index_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "application/index", :type => :view do
+  it 'check if it shows the most recent movie' do
+  	assign(:movie, Movie.create(title: 'gladiator'))
+		render 
+		expect(rendered).to include ('gladiator')
+  end
+end
