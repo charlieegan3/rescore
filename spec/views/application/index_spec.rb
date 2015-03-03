3ea0bc0c46e770f1b7b0eb299508e126e3c9f141
Charlie Egan <charlieegan3@googlemail.com>
2015-03-03 19:08:05 +0000

    Correct whitespace

--- spec/views/application/index_spec.rb
+++ spec/views/application/index_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "application/index", :type => :view do
   it 'check if it shows the most recent movie' do
-  	assign(:movie, Movie.create(title: 'gladiator'))
-		render 
-		expect(rendered).to include ('gladiator')
+    assign(:movie, Movie.create(title: 'gladiator'))
+    render
+    expect(rendered).to include ('gladiator')
   end
 end
