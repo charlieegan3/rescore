ad3af4ac3784d08bd21130405215870b8e1de8b8
Charlie Egan <charlieegan3@googlemail.com>
2015-03-04 19:07:19 +0000

    Correct spec description

--- spec/views/application/index_spec.rb
+++ spec/views/application/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "application/index", :type => :view do
-  it 'it shows the most recent movie' do
+  it 'shows the most recent movie' do
     assign(:movie, Movie.create(title: 'gladiator'))
     render
     expect(rendered).to include ('gladiator')
