d560676926844164d4aa7ca5c3299193c1e01d5d
worldinmyeyes <wojciechszczerek@wp.pl>
2015-03-05 16:26:08 +0000

    added new file containing deleted multiwords only

---
+++ config/initializers/dictionary-multiwords.rb
@@ -0,0 +1,12 @@
+POLARITIES = {  
+  'wellconnected' => 2,
+  'welleducated' => 1,
+  'wellestablished' => 1,
+  'wellinformed' => 2,
+  'wellintentioned' => 2,
+  'wellmanaged' => 2,
+  'wellpositioned' => 1,
+  'wellpublicized' => 1,
+  'wellreceived' => 2,
+  'wellregarded' => 2
+}
