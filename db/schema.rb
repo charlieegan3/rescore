f47892031b37268d9c84bd98652a52b6f6d0de84
Dylan Hanning <dylanhanning123@hotmail.com>
2015-03-09 13:16:35 +0000

    Add category to reports

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309093353) do
+ActiveRecord::Schema.define(version: 20150309131330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20150309093353) do
     t.integer  "movie_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "category"
   end
 
   add_index "reports", ["movie_id"], name: "index_reports_on_movie_id", using: :btree
