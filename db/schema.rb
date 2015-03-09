5d692c515d503976a4b14c8f9cb3736501b682c8
Dylan Hanning <dylanhanning123@hotmail.com>
2015-03-09 11:22:20 +0000

    Add simple 'flag' reports for movies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303205200) do
+ActiveRecord::Schema.define(version: 20150309093353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,15 @@ ActiveRecord::Schema.define(version: 20150303205200) do
     t.string   "slug"
     t.text     "references"
   end
+
+  create_table "reports", force: true do |t|
+    t.text     "description"
+    t.integer  "movie_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "reports", ["movie_id"], name: "index_reports_on_movie_id", using: :btree
 
   create_table "statistics", force: true do |t|
     t.string   "identifier"
