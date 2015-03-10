35a841fe2337adc908e219e398e55da248552845
Dylan Hanning <dylanhanning123@hotmail.com>
2015-03-10 16:01:08 +0000

    Add user field to reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -34,7 +34,7 @@ class ReportsController < ApplicationController
       return redirect_to movie_path(Movie.find(params[:movie_id]))
     end
 
-    @report = Report.create(movie_id: params[:movie_id].to_i, description: params[:description][0..300], category: params[:type])
+    @report = Report.create(movie_id: params[:movie_id].to_i, description: params[:description][0..300], category: params[:type], user_id: params[:user_id])
     flash[:notice] = "Report sent, thanks for your feedback!"
     redirect_to movie_path(Movie.find(params[:movie_id].to_i))
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309131330) do
+ActiveRecord::Schema.define(version: 20150310155756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20150309131330) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "category"
+    t.integer  "user_id"
   end
 
   add_index "reports", ["movie_id"], name: "index_reports_on_movie_id", using: :btree
