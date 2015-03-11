db82299488557b0d052ad4de5770f6738a93ccd5
Dylan Hanning <dylanhanning123@hotmail.com>
2015-03-11 21:07:14 +0000

    Move admin username and password to constant

--- config/initializers/keys.rb
+++ config/initializers/keys.rb
@@ -3,3 +3,5 @@ TWITTER_KEY = '4iXopvjq5m7BzmgmJ4dlw6lMf'
 TWITTER_SECRET = 'Dx4LZnwvtmc9BMQxqJuXY9NIcL0mMB9rtE1WgLyphA15BYdLYs'
 GOOGLE_AJAX_REFERRER = "www.rescore.io"
 USER_AGENT_STRING = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.854.0 Safari/535.2'
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "1234qwer"
