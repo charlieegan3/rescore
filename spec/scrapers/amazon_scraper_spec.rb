9363bff5a3369760777111f15817fd19307d8450
Dylan Hanning <dylanhanning123@hotmail.com>
2015-03-14 16:31:02 +0000

    Test Amazon scraper with 404

--- spec/scrapers/amazon_scraper_spec.rb
+++ spec/scrapers/amazon_scraper_spec.rb
@@ -8,12 +8,18 @@ RSpec.describe AmazonScraper, :type => :feature do
   end
 
   describe 'reviews' do
-    scraper = AmazonScraper.new('http://www.amazon.co.uk/Godfather-DVD-Marlon-Brando/dp/B00CX5Z3R0/ref=sr_1_6?s=dvd&ie=UTF8&qid=1426187572&sr=1-6&keywords=the+godfather', USER_AGENT_STRING, 5, false)
+    amazon_url = 'http://www.amazon.co.uk/Godfather-DVD-Marlon-Brando/dp/B00CX5Z3R0/ref=sr_1_6?s=dvd&ie=UTF8&qid=1426187572&sr=1-6&keywords=the+godfather'
+    scraper = AmazonScraper.new(amazon_url, USER_AGENT_STRING, 5, false)
 
     it 'gets reviews correctly' do
       VCR.use_cassette('amazon_scraper') do
         expect(scraper.reviews).to_not be_nil
       end
+    end
+
+    it 'returns an empty list on 404' do
+      stub_request(:any, amazon_url).to_return(:body => "abc", :status => 404, :headers => { 'Content-Length' => 3 })
+      expect(scraper.reviews).to eq([])
     end
   end
 end
