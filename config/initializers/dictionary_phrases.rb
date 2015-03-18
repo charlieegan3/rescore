c50a0f16b1318fcb2e3d1e838dce64e1604ad2c6
worldinmyeyes <wojciechszczerek@wp.pl>
2015-03-18 15:23:34 +0000

    reviewed dictionary lines 0-3000

---
+++ config/initializers/dictionary_phrases.rb
@@ -0,0 +1,28 @@
+PHRASE_POLARITIES = {  
+  'above average' => 1,
+  'antiAmerican' => -2,
+  'antiIsraeli' => -2,
+  'antiSemites' => -2,
+  'antiUS' => -2,
+  'bestknown' => 2,
+  'bestperforming' => 2,
+  'bestselling' => 1,
+    'betterknown' => 2,
+  'betterthanexpected' => 2,
+    'counterproductive' => -2,
+      'fastgrowing' => 1,
+  'fastpaced' => 1,
+  'fastestgrowing' => 1,
+
+  'well connected' => 2,
+  'well educated' => 1,
+  'well established' => 1,
+  'well informed' => 2,
+  'well intentioned' => 2,
+  'well managed' => 2,
+  'well positioned' => 1,
+  'well publicized' => 1,
+  'well publicised' => 1,
+  'well received' => 2,
+  'well regarded' => 2
+}
