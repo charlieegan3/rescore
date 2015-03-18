51587cea3486b173a1952d334d125d4277ab62c0
Dylan Hanning <dylanhanning123@hotmail.com>
2015-03-18 12:44:04 +0000

    Clean up tests

--- spec/scrapers/imdb_scraper_spec.rb
+++ spec/scrapers/imdb_scraper_spec.rb
@@ -4,26 +4,23 @@ require 'imdb_scraper'
 
 RSpec.describe Imdb_Scraper, :type => :feature do
   describe 'reviews' do
-    scraper = Imdb_Scraper.new('http://www.imdb.com/title/tt0068646/', USER_AGENT_STRING, 5, false)
+    ok_scraper = Imdb_Scraper.new('http://www.imdb.com/title/tt0068646/', USER_AGENT_STRING, 5, false)
+    empty_scraper = Imdb_Scraper.new('', USER_AGENT_STRING, 5, false)
 
     it 'gets reviews correctly' do
       VCR.use_cassette('imdb_scraper') do
-        expect(scraper.reviews).to_not be_nil
+        expect(ok_scraper.reviews).to_not be_nil
       end
     end
 
     it 'returns an empty list on 404' do
       stub_request(:any, 'http://www.imdb.com/title/tt0068646/reviews').to_return(:body => "abc", :status => 404, :headers => { 'Content-Length' => 3 })
-      expect(scraper.reviews).to eq([])
+      expect(ok_scraper.reviews).to eq([])
     end
-  end
 
-  describe 'reviews' do
-    scraper = Imdb_Scraper.new('', USER_AGENT_STRING, 5, false)
-
-    it 'gets empty array for empty URL' do
+    it 'returns an empty array when given no URL' do
       VCR.use_cassette('imdb_scraper') do
-        expect(scraper.reviews).to eq([])
+        expect(empty_scraper.reviews).to eq([])
       end
     end
   end
--- spec/views/movies/index.html.erb_spec.rb
+++ spec/views/movies/index.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "movies/index", :type => :view do
-  it "renders list of movies" do
+  it "renders the list of movies" do
     assign(:movies, [Movie.create(title: 'lord of the rings')])
     render
     expect(rendered).to include('lord-of-the-rings')
