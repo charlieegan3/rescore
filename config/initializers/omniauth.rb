8b4f6d99264272a33275e1df22df8827541de217
Dylan Hanning <dylanhanning123@hotmail.com>
2015-03-22 13:16:46 +0000

    clean up

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, TWITTER_KEY, TWITTER_SECRET
 
   OmniAuth.config.on_failure = Proc.new { |env|
-  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+    OmniAuth::FailureEndpoint.new(env).redirect_to_failure
   }
 end
