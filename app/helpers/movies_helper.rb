5b3ec4d3b73b16e81343ee4ebc317a234ec19e2b
Charlie Egan <charlieegan3@googlemail.com>
2015-04-21 16:46:27 +0100

    Adjust wording and stats content

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -6,15 +6,15 @@ module MoviesHelper
     var = (movie.sentiment[:distribution_stats][:st_dev])
     [].tap do |facts|
       if var >= sentiment_variation
-        facts << "#{movie.title} has a high variation of ratings."
+        facts << "#{movie.title} has high rating variation."
       else
-        facts << "#{movie.title} has a low variation of ratings."
+        facts << "#{movie.title} has low rating variation."
       end
 
       if (movie.related_people[:cast].size + movie.related_people[:directors].size) >= (global_people_count / Movie.count)
-        facts << "Has an above average number of cast members."
+        facts << "Has a larger than average cast."
       else
-        facts << "Has a below average number of cast members."
+        facts << "Has a smaller than average cast."
       end
 
       facts << "#{movie.sentiment[:topics].max_by{|k,v| v}[0].to_s.capitalize} is the most favoured aspect of the movie."
