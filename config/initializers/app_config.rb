5135f60498c7b5dd388f37ba5d92ffbe37671cb3
Charlie Egan <charlieegan3@googlemail.com>
2015-05-30 14:10:46 +0100

    Only load aplication.yml in development

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,4 +1,5 @@
-require 'yaml'
-
-yaml_data = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'application.yml'))).result)
-HashWithIndifferentAccess.new(yaml_data).map { |k,v| ENV[k] = v }
+if Rails.env.development?
+  require 'yaml'
+  yaml_data = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'application.yml'))).result)
+  HashWithIndifferentAccess.new(yaml_data).map { |k,v| ENV[k] = v }
+end
